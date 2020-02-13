97d06a5d1d94617f6962b63786db7c8db6dc0b38
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-02-13 15:24:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Act_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
